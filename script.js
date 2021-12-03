3107d931f8c9c5c98d6e343dc256b9adac65acf8
vandhana91 <85727736+vandhana91@users.noreply.github.com>
2021-12-03 14:44:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,41 @@
+setTimeout(() => {
+    document.querySelector(".container").innerHTML = "<h1>10</h1>";
+    setTimeout(() => {
+      document.querySelector(".container").innerHTML = "<h1>9</h1>";
+      setTimeout(() => {
+        document.querySelector(".container").innerHTML = "<h1>8</h1>";
+  
+        setTimeout(() => {
+          document.querySelector(".container").innerHTML = "<h1>7</h1>";
+              setTimeout(() => {
+              document.querySelector(".container").innerHTML = "<h1>6</h1>";
+              setTimeout(() => {
+                document.querySelector(".container").innerHTML = "<h1>5</h1>";
+                setTimeout(() => {
+                  document.querySelector(".container").innerHTML = "<h1>4</h1>";
+                  setTimeout(() => {
+                    document.querySelector(".container").innerHTML = "<h1>3</h1>";
+                    setTimeout(() => {
+                      document.querySelector(".container").innerHTML =
+                        "<h1>2</h1>";
+                      setTimeout(() => {
+                        document.querySelector(".container").innerHTML =
+                          "<h1>1</h1>";
+                        setTimeout(() => {
+                            document.querySelector(
+                              ".container"
+                            ).innerHTML = `<h1>Happy Independence Day </h1>`;
+                          }, 1000);
+                        }, 1000);
+                      }, 1000);
+                    }, 1000);
+                  }, 1000);
+                }, 1000);
+              }, 1000);
+            }, 1000);
+          }, 1000);
+        }, 1000);
+      }, 1000);
+   
+  
+  
